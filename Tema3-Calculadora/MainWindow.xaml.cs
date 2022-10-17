01bdb2bfcdf683b0c9db912965d2bd1aa45bfc0a
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2022-10-17 18:00:07 +0200

    Estilos en resources de ventana y creacion dinamica de botones

--- Tema3-Calculadora/MainWindow.xaml.cs
+++ Tema3-Calculadora/MainWindow.xaml.cs
@@ -23,7 +23,32 @@ namespace Tema3_Calculadora
         public MainWindow()
         {
             InitializeComponent();
+            creaBotones();
             Texto_TextBlock.Text = "";
+        }
+        private void creaBotones()
+        { 
+            for(int i=1; i<=9; i++)
+            {
+                    
+                    
+                    TextBlock numero = new TextBlock
+                    {
+                        Text = i.ToString()
+                        
+                    };
+
+                    Viewbox viewbox = new Viewbox();
+                    viewbox.Child = numero;
+
+                    Button boton = new Button
+                    {
+                        Content = viewbox,
+                        Tag = i.ToString()
+                    };
+                    boton.Click += addNumero;
+                    botones_UniformGrid.Children.Add(boton);
+            }
         }
 
         private void addNumero(object sender, RoutedEventArgs e)
